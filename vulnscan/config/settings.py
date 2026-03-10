e45efc0821f4724538e312b0a50afbfdd5dcdf69
MoA3lka <Mohamedalka166@gmail.com>
2026-03-10 03:14:37 +0000

    debug true

--- vulnscan/config/settings.py
+++ vulnscan/config/settings.py
@@ -34,7 +34,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = 'django-insecure-^irl+c1mgro+##$q28%xx52y3)451*p8phnat)-2u4t5ss%c(8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
