f001d0f7599dd7972f62d6876e0ccf5fd975bc4f
MoA3lka <Mohamedalka166@gmail.com>
2026-03-10 02:52:32 +0000

    urls.py fix

--- vulnscan/vulnscan_app/urls.py
+++ vulnscan/vulnscan_app/urls.py
@@ -4,13 +4,11 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('login/',views.login_view, name="login"),   
-
-    path('dashboard/',views.dashboard, name="dashboard"),    
-
-    path('start_scan/',views.start_scan, name="start_scan"),   
-    path('Scan_Result/',views.results, name="Scan_Result"),    
-    path('alerts/',views.alerts, name="alerts"),
-    
+    path('', views.login_view, name="home"),
+    path('login/', views.login_view, name="login"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('start_scan/', views.start_scan, name="start_scan"),
+    path('Scan_Result/', views.results, name="Scan_Result"),
+    path('alerts/', views.alerts, name="alerts"),
 ]
 
