5d618c0931146c3c1649d7b0834d93e13cf2b121
MoA3lka <Mohamedalka166@gmail.com>
2026-03-12 02:54:09 +0000

    css fix

--- vulnscan/config/settings.py
+++ vulnscan/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / "static"
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
